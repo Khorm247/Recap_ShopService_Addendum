277757939d8ed9d6dc47df380058143eec3025ef
Khorm247 <44656481+Khorm247@users.noreply.github.com>
2024-02-12 15:11:20 +0100

    code cleanup + formatting

--- src/main/java/service/ShopService.java
+++ src/main/java/service/ShopService.java
@@ -3,6 +3,7 @@ package service;
 import order.Order;
 import order.OrderMapRepo;
 import order.OrderRepo;
+import order.OrderStatus;
 import product.Product;
 import product.ProductRepo;
 
@@ -11,38 +12,37 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ShopService {
+
+    // Fields
     private final ProductRepo productRepo = new ProductRepo();
     private final OrderRepo orderRepo = new OrderMapRepo();
     private final IdService idService = new IdService();
 
+    // Getters
     public List<Product> getProducts() {
         return productRepo.getProducts();
     }
-
     public Product getProduct(String id) {
         return productRepo.getProductById(id);
     }
-
-    public Product addProduct(Product newProduct) {
-        return productRepo.addProduct(newProduct);
+    public Order getOrder(String id) {
+        return orderRepo.getOrderById(id);
     }
-
-    public void addProducts(List<Product> newProducts) {
-        productRepo.addProducts(newProducts);
-    }
-
-    public void removeProduct(String id) {
-        productRepo.removeProduct(id);
-    }
-
     public List<Order> getAllOrders() {
         return orderRepo.getOrders();
     }
 
-    public Order getOrder(String id) {
-        return orderRepo.getOrderById(id);
-    }
+    // #############################################################################################
+    // Methods
+    // #############################################################################################
 
+    // ADD METHODS
+    public Product addProduct(Product newProduct) {
+        return productRepo.addProduct(newProduct);
+    }
+    public void addProducts(List<Product> newProducts) {
+        productRepo.addProducts(newProducts);
+    }
     public Order addOrder(List<String> productIds) {
         if (verifyAndReduceProductStock(productIds)) return null;
 
@@ -50,11 +50,32 @@ public class ShopService {
 
         final BigDecimal totalPrice = calculateTotalPrice(productIds);
         final String orderId = idService.generateId();
-        final Order newOrder = new Order(orderId, products, totalPrice);
+        final Order newOrder = new Order(orderId, products, totalPrice, OrderStatus.PROCESSING);
 
         return orderRepo.addOrder(newOrder);
     }
+    public void addProductsFromCsv(String filePath) {
+        List<Product> products = CsvService.readProductsFromCsv(filePath);
+        productRepo.addProducts(products);
+    }
 
+    // REMOVE METHODS
+    public void removeProduct(String id) {
+        productRepo.removeProduct(id);
+    }
+    public void removeOrder(String id) {
+        orderRepo.removeOrder(id);
+    }
+
+
+    // #############################################################################################
+    // PRIVATE METHODS
+    // #############################################################################################
+
+    // PRODUCT METHODS
+    private boolean isProductAvailable(String productId) {
+        return getProduct(productId).stock() > 0;
+    }
     private boolean verifyAndReduceProductStock(List<String> productIds) {
         for (String productId : productIds) {
             if (!isProductAvailable(productId)) {
@@ -65,27 +86,6 @@ public class ShopService {
         }
         return false;
     }
-
-    public void removeOrder(String id) {
-        orderRepo.removeOrder(id);
-    }
-
-    public void addProductsFromCsv(String filePath) {
-        List<Product> products = CsvService.readProductsFromCsv(filePath);
-        productRepo.addProducts(products);
-    }
-
-    private BigDecimal calculateTotalPrice(List<String> productIds) {
-        BigDecimal totalPrice = BigDecimal.ZERO;
-        List<Product> products = retrieveProducts(productIds);
-
-        for (Product product : products) {
-            totalPrice = totalPrice.add(product.price());
-        }
-
-        return totalPrice;
-    }
-
     private List<Product> retrieveProducts(List<String> productIds) {
         final List<Product> products = new ArrayList<>();
 
@@ -97,17 +97,22 @@ public class ShopService {
                 System.out.println("Product with id " + productId + " not found");
             }
         }
-
         return products;
     }
-
-    private boolean isProductAvailable(String productId) {
-        return getProduct(productId).stock() > 0;
-    }
-
     private void reduceProductStock(String productId) {
         Product product = getProduct(productId);
         Product updatedProduct = new Product(product.id(), product.name(), product.price(), product.stock() - 1);
         productRepo.addProduct(updatedProduct);
+    }
+
+    // ORDER METHODS
+    private BigDecimal calculateTotalPrice(List<String> productIds) {
+        BigDecimal totalPrice = BigDecimal.ZERO;
+        List<Product> products = retrieveProducts(productIds);
+
+        for (Product product : products) {
+            totalPrice = totalPrice.add(product.price());
+        }
+        return totalPrice;
     }
 }
